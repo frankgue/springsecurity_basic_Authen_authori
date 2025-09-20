b1a3e058cafe73271d19b175529d0e9390745796
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2025-09-20 11:57:46 +0100

    first commit

---
+++ src/main/java/com/gkfcsolution/springsecurity_basic_authen_authori/SpringsecurityBasicAuthenAuthoriApplication.java
@@ -0,0 +1,13 @@
+package com.gkfcsolution.springsecurity_basic_authen_authori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityBasicAuthenAuthoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityBasicAuthenAuthoriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gkfcsolution/springsecurity_basic_authen_authori/SpringsecurityBasicAuthenAuthoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkfcsolution.springsecurity_basic_authen_authori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityBasicAuthenAuthoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
